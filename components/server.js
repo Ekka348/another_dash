80668bd62fec679be416c889c844502502f47000
Ekka348 <chyr1675@gmail.com>
2025-08-26 15:33:59 +0300

    Create server.js

---
+++ components/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Раздаем статические файлы (JS, CSS, изображения)
+app.use(express.static(path.join(__dirname)));
+
+// Обрабатываем все GET-запросы, отправляя index.html
+// Это необходимо для работы SPA-роутинга
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
