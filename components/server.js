3f15eb189fe4cbeb7b1cffcd7cc76eefd0e00220
Ekka348 <chyr1675@gmail.com>
2025-08-26 16:26:31 +0300

    Update server.js

--- components/server.js
+++ components/server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
-
-// Раздаем статические файлы (JS, CSS, изображения)
-app.use(express.static(path.join(__dirname)));
-
-// Обрабатываем все GET-запросы, отправляя index.html
-// Это необходимо для работы SPA-роутинга
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
 
 const PORT = process.env.PORT || 8080;
+
+// Раздаём статику из корня репозитория
+app.use(express.static(path.join(__dirname, '..')));
+
+// Все остальные GET-запросы отправляют index.html из корня
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
